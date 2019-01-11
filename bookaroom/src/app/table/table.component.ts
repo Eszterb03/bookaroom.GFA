0fe9332cc224d7088ca9f01893f4ced8632f8ccc
Eszterb03 <eszter.b03@gmail.com>
2019-01-11 11:05:52 +0100

    add(clickMe) change color

--- bookaroom/src/app/table/table.component.ts
+++ bookaroom/src/app/table/table.component.ts
@@ -11,9 +11,13 @@ export class TableComponent implements OnInit {
   constructor() { }
   rooms = rooms;
   time = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  color = '';
 
 
   ngOnInit() {
   }
 
+  clickMe() {
+     this.color = '#3cb878';
+  }
 }
